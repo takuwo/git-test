5031a72b564e8674a1c23e5a11113c0ce24aa1c5
takuwo <takuwolog@hatena.ne.jp>
2017-09-24 11:58:26 +0900

    gulp入れる

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,26 @@
 var gulp = require('gulp'),
     less = require('gulp-less');
     autoprefixer = require('gulp-autoprefixer');
+    plumber = require("gulp-plumber");
 
-var paths = {
-  less: './less/nokoto.less',
-  css_build: './css'
-}
+
+var lessPath = './less/';
+var cssPath = './css/';
 
 gulp.task('less', function() {
-  gulp.src(paths.less)
-      .pipe(less())
-      .pipe(gulp.dest(paths.css_build));
+    return gulp.src(lessPath + 'nokoto.less')
+      .pipe(plumber())
+      .pipe(less({compress: false}))
+      .pipe(gulp.dest(cssPath))
+      .pipe(autoprefixer({
+        browsers: ['last 2 version', 'iOS >= 8.1', 'Android >= 4.4'],
+        cascade: false
+      }));
 });
-
-// var lessDir = './less/';
-// var cssDir = './css/';
-// 
-// var themeName = 'nokoto';
-// 
-// gulp.task('less', function() {
-//     var lessPath = lessDir + themeName +'.less';
-//     var cssPath = cssDir;
-// 
-//     return gulp.src('lessPath')
-//       .pipe(less({compress: false}))
-//       .pipe(gulp.dest(cssPath))
-//       .pipe(autoprefixer({
-//         browsers: ['last 2 version', 'iOS >= 8.1', 'Android >= 4.4'],
-//         cascade: false
-//       }));
-// });
 
 gulp.task('default', function () {
     gulp.watch(
-        ['./less/**/*.less'],
+        [lessPath + '**/*.less'],
         ['less']
     );
 });
